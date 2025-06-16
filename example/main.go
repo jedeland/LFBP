5abe91432e72835a345f9f94a68e9c3cf6b8d122
jed eland <jedeland@live.co.uk>
2025-06-16 22:05:16 +0000

    added more logic for api key

--- example/main.go
+++ example/main.go
@@ -7,15 +7,33 @@ import (
 	"github.com/joho/godotenv" // Import the godotenv package
 )
 
-func main() {
+func getApiKey() string {
+	// check for .env.api_key file in the parent directory
+	if _, err := os.Stat("../.env.api_key"); os.IsNotExist(err) {
+		fmt.Println("No .env.api_key file found in the parent directory")
+
+		// TODO: getApiKeyConsole()
+
+	} else {
+		fmt.Println("Found .env.api_key file in the parent directory")
+	}
+
 	// Load environment variables from .env file
 	err := godotenv.Load("../.env.api_key")
 	if err != nil {
 		fmt.Println("Error loading .env file")
+		return ""
+	}
+	return os.Getenv("API_KEY")
+}
+
+func main() {
+	apiKey := getApiKey()
+	if apiKey == "" {
 		return
 	}
-	apiKey := os.Getenv("API_KEY")
 
 	fmt.Println("API Key:", apiKey)
 
+	// Use the API key for your application logic
 }
